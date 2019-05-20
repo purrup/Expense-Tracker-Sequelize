4fc58db87f425f22c79341e3399935247017f272
Jerry Tsai <purrup27@gmail.com>
2019-05-20 20:18:20 +0800

    revise the filter function to filter date and category at the same time

--- routes/home.js
+++ routes/home.js
@@ -8,42 +8,73 @@ const monthList = require('../month.json').results
 
 router.get('/', authenticated, (req, res) => {
   const month = req.query.month ? req.query.month : false
-  const category = req.query.category ? req.query.category : true
-  console.log(category)
+  const category = req.query.category
+
   const user = User.findByPk(req.user.id)
     .then(user => {
       if (!user) {
         return res.error()
       }
-      Record.findAll({
-        where: { UserId: req.user.id },
-        // where: Sequelize.and(
-        //   { UserId: req.user.id },
-        //   Sequelize.or({ category: req.query.category }, { category: true })
-        // ),
-      }).then(records => {
-        let totalAmount = 0
-        records = records
-          .filter(record => {
-            // 如果有選月份，就將date中的月份slice出來並比對
-            if (month) {
-              console.log(month)
-              return Number(record.date.slice(5, 7)) === Number(month)
-            }
-            return true
-          })
-          .map(record => {
-            totalAmount += parseInt(record.amount, 10)
-            return record
-          })
+      if (category) {
+        //有category，用req.user.id和req.query.category篩選records
+        Record.findAll({
+          where: {
+            UserId: req.user.id,
+            category: req.query.category,
+          },
+        }).then(records => {
+          let totalAmount = 0
+          records = records
+            .filter(record => {
+              // 如果有選月份，就將date中的月份slice出來並比對
+              if (month) {
+                console.log(month)
+                return Number(record.date.slice(5, 7)) === Number(month)
+              }
+              return true
+            })
+            .map(record => {
+              totalAmount += parseInt(record.amount, 10)
+              return record
+            })
 
-        res.render('index', {
-          records,
-          totalAmount,
-          monthList,
-          month,
+          res.render('index', {
+            records,
+            totalAmount,
+            monthList,
+            month,
+          })
         })
-      })
+      } else {
+        // 沒有category 列出全部records
+        Record.findAll({
+          where: {
+            UserId: req.user.id,
+          },
+        }).then(records => {
+          let totalAmount = 0
+          records = records
+            .filter(record => {
+              // 如果有選月份，就將date中的月份slice出來並比對
+              if (month) {
+                console.log(month)
+                return Number(record.date.slice(5, 7)) === Number(month)
+              }
+              return true
+            })
+            .map(record => {
+              totalAmount += parseInt(record.amount, 10)
+              return record
+            })
+
+          res.render('index', {
+            records,
+            totalAmount,
+            monthList,
+            month,
+          })
+        })
+      }
     })
     .catch(error => {
       return res.status(422).json(error)
