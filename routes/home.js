d7e35f8013d22505932ec0b19e1a351e5471f6f5
Jerry Tsai <purrup27@gmail.com>
2019-05-20 21:12:04 +0800

    add category.json and use #each to render selections in index page

--- routes/home.js
+++ routes/home.js
@@ -5,10 +5,12 @@ const User = db.User
 const router = express.Router()
 const { authenticated } = require('../config/auth')
 const monthList = require('../month.json').results
+const categoryList = require('../category.json').results
 
 router.get('/', authenticated, (req, res) => {
   const month = req.query.month ? req.query.month : false
   const category = req.query.category
+  let categoryName = ''
 
   const user = User.findByPk(req.user.id)
     .then(user => {
@@ -38,11 +40,32 @@ router.get('/', authenticated, (req, res) => {
               return record
             })
 
+          switch (category) {
+            case 'utensils':
+              categoryName = '餐飲食品'
+              break
+            case 'shuttle-van':
+              categoryName = '交通出行'
+              break
+            case 'grin-beam':
+              categoryName = '休閒娛樂'
+              break
+            case 'home':
+              categoryName = '家居物業'
+              break
+            case 'pen':
+              categoryName = '其他'
+              break
+          }
+
           res.render('index', {
             records,
             totalAmount,
             monthList,
             month,
+            categoryList,
+            category,
+            categoryName,
           })
         })
       } else {
@@ -66,12 +89,32 @@ router.get('/', authenticated, (req, res) => {
               totalAmount += parseInt(record.amount, 10)
               return record
             })
+          switch (category) {
+            case 'utensils':
+              categoryName = '餐飲食品'
+              break
+            case 'shuttle-van':
+              categoryName = '交通出行'
+              break
+            case 'grin-beam':
+              categoryName = '休閒娛樂'
+              break
+            case 'home':
+              categoryName = '家居物業'
+              break
+            case 'pen':
+              categoryName = '其他'
+              break
+          }
 
           res.render('index', {
             records,
             totalAmount,
             monthList,
             month,
+            categoryList,
+            category,
+            categoryName,
           })
         })
       }
@@ -80,4 +123,5 @@ router.get('/', authenticated, (req, res) => {
       return res.status(422).json(error)
     })
 })
+
 module.exports = router
